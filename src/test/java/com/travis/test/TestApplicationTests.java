6797f96701c7dd224d3dd6a33f9dafdec70e817c
Catt-168 <satpaingthu2000@gmail.com>
2024-08-31 23:59:45 +0630

    feat: added error test

--- src/test/java/com/travis/test/TestApplicationTests.java
+++ src/test/java/com/travis/test/TestApplicationTests.java
@@ -2,12 +2,30 @@ package com.travis.test;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import java.sql.SQLOutput;
 
 @SpringBootTest
 class TestApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!.... This is Tests");
+	}
+
+	@Test
+	public void runTest(){
+		System.out.println("This is a custom text");
+	}
+
+	@Test
+	public void handleTest(){
+		System.out.println("This task will fail!");
+		Assert.hasLength("","st");
 	}
 
 }
